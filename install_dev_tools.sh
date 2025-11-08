5de06adb6b1af1115225b3de539b2e1f9305fc59
ekatsiashvili <katsiashvili.elena@gmail.com>
2025-11-08 14:35:40 +0200

    Add Bash script for installing Docker, Docker Compose, Python, and Django

---
+++ install_dev_tools.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Debian and Ubuntu
+
+export  DEBIAN_FRONTEND=noninteractive
+
+apt-get update -y
+
+if ! command -v curl &> /dev/null; then
+    echo "Install curl..."
+    sudo apt-get install -y curl
+fi
+
+if ! command -v docker &> /dev/null; then
+  echo "Install Docker..."
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  rm get-docker.sh
+else
+  echo "Docker already installed"
+fi
+
+if ! command -v docker-compose &> /dev/null; then
+  echo "Install Docker Compose..."
+  sudo apt-get install -y docker-compose
+else
+  echo "Docker Compose already installed"
+fi
+
+if ! python3 --version | grep -q '3\.[9-9]\|[1-9][0-9]'; then
+  echo "Install Python 3.9+..."
+  sudo apt-get install -y python3 python3-pip
+else
+  echo "Python 3.9+ already installed"
+fi
+
+if ! command -v pip3 &> /dev/null; then
+  echo "Install pip3..."
+  sudo apt-get install -y python3-pip
+else
+  echo "pip3 already installed"
+fi
+
+if ! python3 -m django --version &> /dev/null; then
+  echo "Install Django..."
+  sudo apt-get install -y python3-django
+else
+  echo "Django already installed"
+fi
+
+echo "Installation complete!"
